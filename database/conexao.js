85742b7e3f609c7458dfc289708113124de9604f
unknown <felipe.almeida@sptech.school>
2023-05-17 04:51:53 -0300

    Não faço ideia do que tá rolando :(

--- database/conexao.js
+++ database/conexao.js
@@ -1,12 +1,11 @@
-const mysql = require('mysql');
+const mysql = require('mysql12');
 
 // Sua conexão com o banco de dados
 const conexao = mysql.createConnection({
-    host:'localhost', // O seu host 
-    port: 3306, // A porta que o MySQL está usando. A 3306 é a padrão do mysql
-    user: 'root', // Seu usuário
-    password: 'passmysql', // Sua senha para este usuário
-    database: 'bdAtendimento' // Nome do banco e dados que deseja usar
+    host: "localhost",
+    database: "bdAtendimento",
+    user: "root",
+    password: "passmysql" 
 });
 
 // Criando modulo para ser exportado lá para o index.js (o que está FORA da pasta de rotas) :)
